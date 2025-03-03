ed061be6268937406bc9320a4d639905eeabd545
tatiperez59 <141940722+tatiperez59@users.noreply.github.com>
2025-03-03 13:28:10 -0500

    Update Patio.java
    atributos de patio y su constructor

--- Trasmilenio/src/SistemaTrasmilenio/Patio.java
+++ Trasmilenio/src/SistemaTrasmilenio/Patio.java
@@ -1,4 +1,18 @@
 
 public class Patio {
+      private String ubicacion;
+	    private int cantidadBuses;
 
+	    public Patio(String ubicacion, int ubicacion) {
+	        this.ubicacion = ubicacion;
+	        this.cantidadBuses = cantidadBuses;
+	    }
+
+	    public String getUbicacion() {
+	        return nombre;
+	    }
+
+	    public int getCantidadBuses() {
+	        return capacidad;
+	    }
 }
