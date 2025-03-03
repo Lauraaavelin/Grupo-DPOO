683960547743707c07f049a3da331072da3893c7
tatiperez59 <141940722+tatiperez59@users.noreply.github.com>
2025-03-03 14:09:13 -0500

    Update Torniquete.java
    atributos de torniqute

--- Trasmilenio/src/SistemaTrasmilenio/Torniquete.java
+++ Trasmilenio/src/SistemaTrasmilenio/Torniquete.java
@@ -1,5 +1,17 @@
 package SistemaTrasmilenio;
 
 public class Torniquete {
-
+      private String identificador;
+	    private String entrada;
+      private String salida;
+	    private double saldo;
+      private double diferencia;
+      public Torniquete(String identificador,String entrada,String salida,
+                        double saldo,double diferencia) {
+	        this.identificador = identificador;
+	        this.entrada = entrada;
+          this.salida = salida;
+          this.saldo = saldo;
+          this.diferencia = diferencia;
+	    }
 }
