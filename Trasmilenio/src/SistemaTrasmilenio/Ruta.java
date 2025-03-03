aace75d92b845abecc8164fcfb80515ee6c737d8
tatiperez59 <141940722+tatiperez59@users.noreply.github.com>
2025-03-03 13:35:13 -0500

    Update Ruta.java
    atributos de ruta

--- Trasmilenio/src/SistemaTrasmilenio/Ruta.java
+++ Trasmilenio/src/SistemaTrasmilenio/Ruta.java
@@ -1,4 +1,11 @@
 
 public class Ruta {
-
+      private String numeroIdentificador;
+      private String horario;
+	    private String destino;
+      private String origen;
+      private List<Estacion> estaciones;
+  
+	    
+	
 }
