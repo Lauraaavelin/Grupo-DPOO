742650a96e4b430aedd582f4b540566dffe937e2
tatiana_perez <k.perezs@uniandes.edu.co>
2025-03-05 09:17:16 -0500

    commit de bus

--- Trasmilenio/src/SistemaTrasmilenio/Bus.java
+++ Trasmilenio/src/SistemaTrasmilenio/Bus.java
@@ -1,5 +1,32 @@
 package SistemaTrasmilenio;
 
 public abstract class Bus {
+		
+		private int capacidad;
+		private String identifiador;
+		private String conductor;
+		private String estado;
+		public Bus(int capacidad, String identifiador, String conductor, String estado) {
+			super();
+			this.capacidad = capacidad;
+			this.identifiador = identifiador;
+			this.conductor = conductor;
+			this.estado = estado;
+		}
+		public int getCapacidad() {
+			return capacidad;
+		}
+		public String getIdentifiador() {
+			return identifiador;
+		}
+		public String getConductor() {
+			return conductor;
+		}
+		public String getEstado() {
+			return estado;
+		}
+		
+		
+		
 
 }
