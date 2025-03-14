d12a89e01ece86e93627a5da49dce9539f17886a
l.avelino@uniandes.edu.co <l.avelino@uniandes.edu.co>
2025-03-14 08:11:41 -0500

    comit

--- Trasmilenio/src/SistemaTrasmilenio/Aseo.java
+++ Trasmilenio/src/SistemaTrasmilenio/Aseo.java
@@ -2,6 +2,11 @@ package SistemaTrasmilenio;
 
 public class Aseo extends Trabajador{
 
+	public Aseo(String nombre, int iD) {
+		super(nombre, iD);
+		// TODO Auto-generated constructor stub
+	}
+
 
 
 	
